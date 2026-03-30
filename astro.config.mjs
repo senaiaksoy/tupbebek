c4ce2219b9e9ab4d587d5f7bb9cc9d57ee1e82d3
senai aksoy <senaiaksoy@hotmail.com>
2026-03-30 21:15:22 +0300

    ads

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,22 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, squooshImageService } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://tupbebek.com",
-  output: "static",
+  output: "hybrid",
   integrations: [mdx(), sitemap()],
+
   image: {
     domains: ["tupbebek.com", "img.youtube.com"],
     service: squooshImageService(),
   },
+
   build: {
     inlineStylesheets: "always",
   },
+
+  adapter: cloudflare()
 });
